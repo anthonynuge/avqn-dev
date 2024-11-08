725a5df44b1444bb980241e74e37b72a4ec7fd0f
antho <anthonynuge0509@gmail.com>
2024-11-08 15:25:51 -0600

    Remove @next/font and replace with built in.

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode } from "react";
-import { Inter } from "@next/font/google";
+import { Inter } from "next/font/google";
 import styles from "./layout.module.css";
 
 const inter = Inter({
