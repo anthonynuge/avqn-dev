e83be20d3389582a5735a3855abca0a64354963e
antho <anthonynuge0509@gmail.com>
2024-11-14 17:39:59 -0600

    Add overlay fade in and out animation.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,46 +5,65 @@ import React, { useState } from "react";
 import styles from "@styles/Navbar.module.css";
 
 const Navbar = () => {
-  const [isActive, setIsActive] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const [isFadingOut, setIsFadingOut] = useState(false);
 
   const toggleMenu = () => {
-    setIsActive(!isActive);
+    // setIsOpen(!isOpen);
+    if (isOpen) {
+      setIsFadingOut(true);
+      setTimeout(() => {
+        setIsOpen(false);
+        setIsFadingOut(false);
+      }, 500);
+    } else {
+      setIsOpen(true);
+    }
   };
 
   return (
     // <div>Navbar</div>
-    <nav className={`${styles.navbar} ${isActive ? styles.active : ""}`}>
+    <nav className={styles.navbar}>
       <div className={styles.logo}>
         <Link href="/">Logo</Link>
       </div>
       <div className={styles.hamburgerToggle} onClick={toggleMenu}>
         <span
-          className={`${styles.hamBar} ${isActive ? styles.xTop : ""}`}
+          className={`${styles.hamBar} ${isOpen ? styles.xTop : ""}`}
         ></span>
         <span
-          className={`${styles.hamBar} ${isActive ? styles.xMid : ""}`}
+          className={`${styles.hamBar} ${isOpen ? styles.xMid : ""}`}
         ></span>
         <span
-          className={`${styles.hamBar} ${isActive ? styles.xBot : ""}`}
+          className={`${styles.hamBar} ${isOpen ? styles.xBot : ""}`}
         ></span>
       </div>
-      <ul className={styles.navLinks}>
-        <li className={styles.navItem}>
-          <Link href="/about" className={styles.navLink}>
-            About
-          </Link>
-        </li>
-        <li className={styles.navItem}>
-          <Link href="/projects" className={styles.navLink}>
-            Projects
-          </Link>
-        </li>
-        <li className={styles.navItem}>
-          <Link href="/contact" className={styles.navLink}>
-            Contact
-          </Link>
-        </li>
-      </ul>
+      {isOpen && (
+        // <div className={styles.navOverlay}>
+        <div
+          className={`${styles.navOverlay} ${
+            isFadingOut ? styles.fadeOut : styles.fadeIn
+          } `}
+        >
+          <ul className={styles.navLinks}>
+            <li className={styles.navItem}>
+              <Link href="/about" className={styles.navLink}>
+                About
+              </Link>
+            </li>
+            <li className={styles.navItem}>
+              <Link href="/projects" className={styles.navLink}>
+                Projects
+              </Link>
+            </li>
+            <li className={styles.navItem}>
+              <Link href="/contact" className={styles.navLink}>
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </div>
+      )}
     </nav>
   );
 };
