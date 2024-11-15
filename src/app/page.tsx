e559e8a98534cfd2064ce0b2b24206ef8f77dcde
antho <anthonynuge0509@gmail.com>
2024-11-14 19:55:31 -0600

    Add social links.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import styles from "@styles/page.module.css";
+import Socials from "@app/components/Socials";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>Hello World</main>
+      <Socials />
       <footer className={styles.footer}></footer>
     </div>
   );
