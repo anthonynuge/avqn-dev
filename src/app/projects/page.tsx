36c226e745040b99dae81f65b0c4d79712ed5684
antho <anthonynuge0509@gmail.com>
2024-11-28 00:13:50 -0600

    Implement demo gif on hover.

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import styles from "./projects.module.css";
 import { projects } from "@data/projectsData";
--- src/data/projectsData.ts
+++ src/data/projectsData.ts
@@ -1,20 +1,30 @@
 export const projects = [
   {
+    title: "Maze Solver",
+    description:
+      "Built in Java, this maze solver app was created to explore and learn pathfinding algorithms. It visualizes the process of finding the optimal path through a maze, offering an interactive way to understand algorithms like A* and BFS.",
+    image: "/covers/maze.webp",
+    gif: "/gifs/demo.gif",
+    link: "https://myportfolio.com",
+    tags: ["Java", "JavaSwing", "JavaFX"],
+    git: "github.com",
+  },
+  {
     title: "Portfolio Website",
     description:
       "A sleek personal portfolio showcasing my work and skills. Hire me!",
-    image: "/images/portfolio.png",
+    image: "/covers/maze.webp",
     gif: "/gifs/portfolio.webp",
     link: "https://myportfolio.com",
     tags: ["React", "Next.js", "TypeScript", "Javascript", "CSS", "HTML"],
-    git: "",
+    git: "github.com",
   },
   {
     title: "Fruit Vision",
     description:
       "Neural net trained to identify different fruits from images. For the times that your unsure if that is a apple or kiwi.",
     image: "/images/portfolio.png", // Replace with the correct image path
-    gif: "/gifs/fruit.webp",
+    gif: "/gifs/fruitclassifier.gif",
     link: "https://myportfolio.com",
     tags: [
       "Python",
